326d28f88a2772185969fd0d33c902c3fd62ef74
neko senpai <126377692+nekosenpai007@users.noreply.github.com>
2025-05-07 10:46:51 +0530

    Update script.js
    update the emailjs on the js script

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,18 +39,16 @@ $(document).ready(function () {
 
     // <!-- emailjs to mail contact form data -->
     $("#contact-form").submit(function (event) {
-        emailjs.init("service_3cv5ga3");
-
-        emailjs.sendForm('service_3cv5ga3', 'template_r4fp27a', '#contact-form')
-            .then(function (response) {
-                console.log('SUCCESS!', response.status, response.text);
-                document.getElementById("contact-form").reset();
-                alert("Form Submitted Successfully");
-            }, function (error) {
-                console.log('FAILED...', error);
-                alert("Form Submission Failed! Try Again");
-            });
-        event.preventDefault();
+    event.preventDefault();
+    emailjs.sendForm('service_3cv5ga3', 'template_r4fp27a', '#contact-form')
+        .then(function (response) {
+            console.log('SUCCESS!', response.status, response.text);
+            document.getElementById("contact-form").reset();
+            alert("Form Submitted Successfully");
+        }, function (error) {
+            console.log('FAILED...', error);
+            alert("Form Submission Failed! Try Again");
+        });
     });
     // <!-- emailjs to mail contact form data -->
 
